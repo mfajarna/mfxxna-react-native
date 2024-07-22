fec9e4e145579f688ac9a18b749cd499634e2516
muhammad fajar <invasionfajar@gmail.com>
2024-07-22 13:16:33 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Landing Page soon</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
